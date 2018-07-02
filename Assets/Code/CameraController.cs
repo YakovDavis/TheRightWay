8a3c702f93a40b7890be1dac574112705387c482
Yakov Dav <davyakov@gmail.com>
2018-07-02 18:27:11 +0300

    Added at least something for real this time

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+    {
+
+    public bool followPlayer;
+    public int scale;
+
+    GameObject player;
+    Camera orthcamera;
+
+    void Start()
+        {
+        player = GameObject.Find("Player");
+        orthcamera = GetComponent<Camera>();
+        //orthcamera.orthographicSize = 1080 / (32 * 2 * scale);
+        }
+
+    void Update()
+        {
+		if (followPlayer)
+            {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+            }
+	    }
+}
